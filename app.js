65a80dc251efa86556c998a6fab37b4d559a440c
ahmed shah <idahmed226@gmail.com>
2023-12-14 10:39:57 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,69 @@
+// var i=0;
+// var pera = document.getElementsByTagName('p');
+//    for(var i=0;i<=pera.length;i++){
+//     pera[i].style.color ="Red"
+//     pera[i].style.fontWeight = 600
+//     pera[i].style.background = 'yellow'    
+//     pera[i].style.fontFamily = 'Arial'
+//     console.log(pera)
+//    }
+
+
+
+
+function changes(){
+    var now = document.getElementById('now');
+    var pera = document.getElementById('pera')
+    pera.style.color='black'
+    pera.style.fontFamily='Arial'
+    pera.style.background='skyblue'
+    pera.style.fontWeight = 900
+    pera.style.fontsize = '200px'
+
+}
+
+
+
+
+function other(){
+    var people = document.getElementById('people');
+    var pera2 = document.getElementById('pera2')
+    pera2.style.color='black'
+    pera2.style.fontFamily='Arial'
+    pera2.style.background='red'
+    pera2.style.fontWeight = 900
+    pera2.style.fontsize = '200px'
+
+}
+
+
+
+
+function button(){
+    var making = document.getElementById('making');
+    var pera3 = document.getElementById('pera3')
+    pera3.style.color='black'
+    pera3.style.fontFamily='Arial'
+    pera3.style.background='green'
+    pera3.style.fontWeight = 900
+    pera3.style.fontsize = '200px'
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var pera = document.getElementsByTagName('p');
+
+// var pera = document.lastChild.childNodes[2];
+// console.log(pera);
+
